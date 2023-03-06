77d5348449d9f0de87b8b55b7238e6f7f1911d8d
Jonatan Kwiatkowski <joniqs91@gmail.com>
2023-03-06 14:00:56 +0000

    Fixed indentation problem.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -24,35 +24,35 @@ function generateMarkdown(data) {
   
     return `# ${data.title}
   
-    ## Description
-    ${data.description}
+  ## Description
+  ${data.description}
     
-    ## Table of Contents
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [License](#license)
-    - [Contributing](#contributing)
-    - [Tests](#tests)
-    - [Questions](#questions)
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
     
-    ## Installation
-    ${data.installation}
+  ## Installation
+  ${data.installation}
     
-    ## Usage
-    ${data.usage}
+  ## Usage
+  ${data.usage}
     
-    ## License
-    [![License](${licenseBadgeURL})](https://opensource.org/licenses/${encodeURIComponent(data.license)})
-    This project is licensed under the ${data.license} license.
+  ## License
+  [![License](${licenseBadgeURL})](https://opensource.org/licenses/${encodeURIComponent(data.license)})
+  This project is licensed under the ${data.license} license.
     
-    ## Contributing
-    ${data.contribution}
+  ## Contributing
+  ${data.contribution}
     
-    ## Tests
-    ${data.tests}
+  ## Tests
+  ${data.tests}
     
-    ## Questions
-    If you have any questions, please feel free to contact me via email at ${data.email}, or visit my [GitHub profile](https://github.com/${data.github}).`;
+  ## Questions
+  If you have any questions, please feel free to contact me via email at ${data.email}, or visit my [GitHub profile](https://github.com/${data.github})`;
   }
   
   module.exports = generateMarkdown;
